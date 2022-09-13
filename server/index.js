f967441cffb3baea6c827a62c3ff59beb1198eee
mudassirwaseem <mudassirwaseem24@gmail.com>
2022-09-13 14:23:52 +0500

    remove extra console

--- server/index.js
+++ server/index.js
@@ -18,14 +18,11 @@ io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
-    console.log(data, "room====");
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
   socket.on("send_message", (data) => {
-    console.log(data, "send_message====");
-
     socket.to(data.room).emit("receive_message", data);
   });
 
